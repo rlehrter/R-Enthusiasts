ce8629a815178cd766a8943d36dfec94445ab921
Ricky Lehrter <rlehrter@battelleecology.org>
2022-01-26 10:21:40 -0700

    Altered annotations to share with others

--- HotchkissCO2_dataviewscript.R
+++ HotchkissCO2_dataviewscript.R
@@ -4,14 +4,18 @@
 ###GET THE DATA
 #First navigate to data location on your PC here: C:\Campbellsci\LoggerNet
 #Open the CR1000_Table1.dat (for S1) OR CR1000_2_Table2.dat (for S2) file
+##Your file names may be different depending on how you named your tables during configuration
 #Save as, changing the suffix to a .csv and save to your working directory
 
+library(dplyr)
 library(lubridate)
+library(ggplot2)
+library(plotly)
 
 ###LOAD THE DATA
 CO2_data <- read.csv("C:/Users/rlehrter/Desktop/R Working Directory/CR1000_2_Table1.csv")
 
-###WRANGLE THE DATA (tidy er up)
+###WRANGLE THE DATA
 #Get rid of column names that don't make sense and view dataframe again to make sure it worked
 names(CO2_data) <- NULL
 View(CO2_data)
@@ -22,7 +26,7 @@ names(CO2_data) <- CO2_data[1,]
 CO2_data <- CO2_data[-1,]
 CO2_data <- CO2_data[,-c(7,8)]
 
-#New function, this turned out awesome! Get that lubridate cheat sheet
+#New function from the lubridate cheatsheet
 CO2_data$DateHour <- floor_date(as.POSIXct(CO2_data$TIMESTAMP), unit = "hour") 
 CO2_data$DateHour <- as.POSIXct(CO2_data$DateHour)
 CO2_data$GP_CO2Conc <- as.numeric(CO2_data$GP_CO2Conc)
@@ -50,8 +54,6 @@ ggplot(CO2_data_2, aes(x = DateHour, y = GP_CO2Conc))+
   labs(x = "", y = "CO2 Concentration (ppm)", title = "CO2 Concentration at COMO S2 (Hotchkiss AA)")
 
 ###Let's try an interactive plot with plotly
-library(plotly)
-
 CO2_dataplot <- plot_ly(CO2_data_2, x = ~DateHour, y = ~GP_CO2Conc, type = 'scatter', mode = 'lines')
 
 CO2_dataplot_clean <- CO2_dataplot %>% 
